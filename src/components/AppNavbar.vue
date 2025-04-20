bfcca3d9d28f8724359d90f336409f30a6159332
Alexander Morugin <nobilis@bk.ru>
2025-04-20 18:16:31 +0300

    сделал шаблоны страниц

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="navbar">
+    <router-link to="/"><h3>Список задач</h3></router-link>
+
+    <ul class="navbar-menu">
+      <li><router-link to="/tasks">Все задачи</router-link></li>
+      <li>
+        <router-link to="/new">Создать задачу</router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
