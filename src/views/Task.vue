2a8f44626e213070a07b92dfedfc4624479ee69a
Alexander Morugin <nobilis@bk.ru>
2025-04-21 17:13:06 +0300

    работают кнопки изменения статуса задачи

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="card" v-if="currentTask">
     <h2>{{ currentTask.title }}</h2>
-    <p><strong>Статус</strong>: <AppStatus :type="currentTask.status" /></p>
+    <p><strong>Статус</strong>: <AppStatus :type="statusInfo" /></p>
     <p><strong>Дэдлайн</strong>: {{ currentTask.date }}</p>
     <p><strong>Описание</strong>: {{ currentTask.description }}</p>
     <div>
-      <button class="btn">Взять в работу</button>
-      <button class="btn primary">Завершить</button>
-      <button class="btn danger">Отменить</button>
+      <button class="btn" @click="changeStatus('pending')">
+        Взять в работу
+      </button>
+      <button class="btn primary" @click="changeStatus('done')">
+        Завершить
+      </button>
+      <button class="btn danger" @click="changeStatus('cancelled')">
+        Отменить
+      </button>
     </div>
   </div>
   <h3 class="text-white center" v-else>
@@ -20,6 +26,7 @@ import AppStatus from '../components/AppStatus.vue';
 import { useRoute } from 'vue-router';
 import { useStore } from 'vuex';
 import { computed } from 'vue';
+import axios from 'axios';
 
 export default {
   setup() {
@@ -30,9 +37,40 @@ export default {
     const currentTask = computed(() =>
       store.getters.tasks.find((el) => el.id === currentId)
     );
+    let statusInfo = computed(() => currentTask.value.status);
+
+    const changeStatus = async (args) => {
+      try {
+        await axios.patch(
+          `https://vue-freelance-df1a9-default-rtdb.firebaseio.com/task/${currentId}.json`,
+          { status: args }
+        );
+
+        setTimeout(() => {
+          getStatus();
+        }, 200);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getStatus = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://vue-freelance-df1a9-default-rtdb.firebaseio.com/task/${currentId}.json`
+        );
+
+        currentTask.value.status = data.status;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     return {
       currentId,
       currentTask,
+      statusInfo,
+      changeStatus,
     };
   },
   components: { AppStatus },
